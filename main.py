fb7003f1bdfc1aced212dfaa5f4e51c6efce54f7
mach1narium <56261704+mach1narium@users.noreply.github.com>
2022-09-18 09:20:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Game:
 		# setup
 		pygame.init()
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-		pygame.display.set_caption('Flappy Sammy')
+		pygame.display.set_caption('Swimmy Sammy')
 		self.clock = pygame.time.Clock()
 		self.active = False
 
