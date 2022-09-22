7f652a05f99d2c099ed1c8cbacacbb9ff33f17cf
mach1narium <56261704+mach1narium@users.noreply.github.com>
2022-09-22 12:23:32 +0200

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -189,12 +189,12 @@ class ParticleBubble(pygame.sprite.Sprite):
 		if self.rect.right <= -50:
 			self.kill()
 
-	def emit(self):
+	def emit(self,dt):
 		# moves and draws the particles
 		if self.particles:
 			self.delete_particles()
 			for particle in self.particles:
-				particle[0][0] -= particle[2][0]
+				particle[0][0] -= 450 * dt
 				particle[0][1] -= particle[2][1]
 				particle[1] -= 0.05
 				#particle[3] -= 0.2
